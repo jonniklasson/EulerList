f994e0b2533f4fffa5a12f031ae267e19cb72796
Jonathan <jonathan.niklasson@gmail.com>
2015-10-06 19:55:18 +0200

    adding solution 6-8

---
+++ euler6.py
@@ -0,0 +1,7 @@
+sqrSum  = 0
+sumtSqr = 0
+
+for v in range(101):
+    sqrSum  += v**2
+    sumtSqr += v
+print(sumtSqr**2 - sqrSum)
---
+++ euler8/euler8.py
@@ -0,0 +1,40 @@
+'''
+Load a file with a string of numbers
+read 13 numbers, save the product
+increase index by one if product is greater than
+previous saved replace.
+Continue until string is consumed
+'''
+
+'''
+Modify read file function for new file.
+
+'''
+
+
+'''
+string[n:13+n]
+from n = 0 untill n = 1000-13
+if 0 is in number return 0
+'''
+import readfile
+
+def product (string):
+    val = 1
+    for v in string:
+        if int(v) == 0:
+            return 0
+        val *= int(v)
+    return val
+
+numbers = readfile('numbersEuler8.txt')
+saved   = 0
+for n in range(0,987+1):
+    prod = product(numbers[n:13+n])
+    if prod > saved:
+        saved = prod
+    
+    
+
+
+
